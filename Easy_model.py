309792b7eb5684930dcf7bf44b4e03c66bfdb0a3
Guannan Guo <plumberguo1231@gmail.com>
2023-11-16 21:37:52 -0500

    Initialize

---
+++ Easy_model.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 20:58:49 2023
+
+@author: www92
+"""
+
+import numpy as np
+
+x = np.arange(1,11,1)
+p = np.arange(-5,5,1)
+
+print(len(x))
+print(len(p))
